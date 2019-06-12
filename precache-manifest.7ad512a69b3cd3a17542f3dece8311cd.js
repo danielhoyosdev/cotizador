597a563e165887943fd6c7c68cd76833c85a18cf
Daniel Hoyos <danielhoyos350@gmail.com>
2019-06-11 19:24:34 -0500

    Updates

---
+++ precache-manifest.7ad512a69b3cd3a17542f3dece8311cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ebca1419dc1569c15327e5836e2823",
+    "url": "/cotizador/index.html"
+  },
+  {
+    "revision": "1407bdaa26141596a3c6",
+    "url": "/cotizador/static/css/main.4016eb16.chunk.css"
+  },
+  {
+    "revision": "3de0ac43351c923b423f",
+    "url": "/cotizador/static/js/2.abbd2b59.chunk.js"
+  },
+  {
+    "revision": "1407bdaa26141596a3c6",
+    "url": "/cotizador/static/js/main.e6bf7daf.chunk.js"
+  },
+  {
+    "revision": "9d1dc2de9aa60c4a23b0",
+    "url": "/cotizador/static/js/runtime~main.ac779f68.js"
+  }
+]);
